9801b9ffd844be109bb1e40c2b663dced9c6f077
blakley <blakley.ent@gmail.com>
2021-08-01 02:58:05 -0400

    Created a text output handler Created a text input handler Updated README Added text to clipboard

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Transcriber
-Python audio transcriber tool
+
+Audio transcriber tool
+
+## Features
+
+* Audio transcription
+* Saves output to a txt file `{my_audio_file}-transcription.txt` as well as the clipboard
+* Supports both mp3 and wav audio files
+
+## Dependencies
+
+Have ffmpeg installed on your system
+Install the following using `pip`
+```
+$ pip install pydub
+$ pip install ffmpeg
+$ pip install pyperclip
+$ pip install SpeechRecognition
+```
+
+## Usage
+
+Run the tool by providing it with an audio file:
+
+```
+$ python transcribe.py my_audio_file.wav
+```
--- src/transcribe.py
+++ src/transcribe.py
@@ -1,36 +1,59 @@
-'''
-dependencies
-pip install SpeechRecognition
-pip install pydub
-
-description
-Provide it with the audio file and it'll
-output the text to an audio.txt file
-maybe run it through the program a few times and select the text that has the most matches: make some type of accuracy measurement
-
-format
-WAV: must be in PCM/LPCM format
-'''
-
-import pprint
+import sys
+# import pprint
+import pyperclip as clipboard
 from pydub import AudioSegment
 import speech_recognition as sr
 
-r = sr.Recognizer()
+class Transcribe():
+	def __init__(self):
+		self.r = sr.Recognizer()		
+		self.name = ""
+		self.init()
+				
+	# starter function
+	def init(self):
+		line = '---------------------------------'
+		print(line)
+		print("\tAudio Transcriber")
+		print(line + '\n')
+		
+		# check file extension
+		f = open(sys.argv[1], 'r')
+		self.file_name = f.name[2:]
+		if f.name.endswith('mp3'):
+			self.convert()
+		
+		# save source reference
+		self.source = sr.AudioFile(self.file_name)
+		f.close()
+		self.listen_google()
 
-jackhammer = sr.AudioFile('../audio/jackhammer.wav')
-with jackhammer as source:
-	r.adjust_for_ambient_noise(source, duration=0.5)
-	audio = r.record(source)
+	# listens and converts audio to text
+	def listen_google(self):
+		with self.source as source:
+			self.r.adjust_for_ambient_noise(source)
+			audio = self.r.record(source)
 
-def rec():
-	text = r.recognize_google(audio, show_all=True)
-	pp = pprint.PrettyPrinter(depth=6)
-	pp.pprint(text)
-	# print(text)
+		# self.text = self.r.recognize_google(audio, show_all=True)
+		# pp = pprint.PrettyPrinter(depth = 6)
+		# pp.pprint(self.text)
 
-def mp3_to_wav():
-	sound = AudioSegment.from_mp3("/path/to/file.mp3")
-	sound.export("/output/path/file.wav", format="wav")
+		self.text = self.r.recognize_google(audio)
+		self.output()
 
-rec()
+	# converts mp3 audio to wav
+	def convert(self):	
+		sound = AudioSegment.from_mp3(self.file_name)
+		dest = self.file_name[:-4] + '.wav'
+		sound.export(dest, format="wav")
+		os.remove(self.file_name)
+		self.file_name = dest
+	
+	# outputs text to a file and to clipboard
+	def output(self):
+		clipboard.copy(self.text)
+		audio_file = open(f"{self.file_name[:-4]}-transcription.txt", "w")
+		audio_file.write(self.text)
+		audio_file.close()
+
+audio = Transcribe()
